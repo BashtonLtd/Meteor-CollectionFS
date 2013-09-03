b4b35da96feb510cb479b743a4fed354434ff569
Eric Dobbertin <eric@dairystatedesigns.com>
2013-09-03 11:16:29 -0500

    0.6.5 compatibility

---
+++ package.js
@@ -0,0 +1,31 @@
+Package.describe({
+  summary: "Filesystem for Meteor, collectionFS"
+});
+
+Package.on_use(function(api) {
+  api.use(['deps', 'underscore', 'templating', 'handlebars']);
+  
+  if (typeof api.export !== "undefined") {
+    api.export(['CollectionFS', 'CFSErrorType']);
+  }
+
+  api.add_files([ 'myConsole.js' ], [ 'client', 'server' ]);
+
+  api.add_files([
+            'FileSaver.js',
+          'collectionFS_templates.html',
+          'collectionFS_client.js',
+          'collectionFS_client.api.js',
+          'collectionFS_handlebars.js'], 'client');
+  
+  api.add_files([
+  				'collectionFS_filesystem.js',
+  				'collectionFS_server.js',
+  				'collectionFS_filehandlers.js',
+          'collectionFS_server.api.js'], 'server');
+  
+  api.add_files([
+          'collectionFS_common.js',
+          'numeral.js'], ['client', 'server']);
+
+});
